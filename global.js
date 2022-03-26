e3891bfdf1d5fbce32abb3e39bd91610cd70028e
UtterDonkey <68461677+UtterDonkey@users.noreply.github.com>
2022-03-26 19:16:27 +0000

    Update global.js

--- global.js
+++ global.js
@@ -4,3 +4,7 @@ document.body.addEventListener('mousemove', function(e){
   globalValues.mouseX = e.clientX-(window.screenX/2);
     globalValues.mouseY = e.clientY-(window.screenY/2);
 });
+
+function getDirection(startX, startY, endX, endY){
+ return ((Math.atan((endX-startX)/(endY-startY))*180)/Math.PI)-(180*(endY<startY));
+}
