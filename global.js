e6e4623c3a43ab734e2e412b0e91659dc6fe72c0
UtterDonkey <68461677+UtterDonkey@users.noreply.github.com>
2022-03-27 14:43:05 +0100

    Update global.js

--- global.js
+++ global.js
@@ -11,3 +11,13 @@ document.body.addEventListener('mousemove', function(e){
 function getDirection(startX, startY, endX, endY){
   return ((Math.atan((endX-startX)/(endY-startY))*180)/Math.PI)-(180*(endY<startY));
 };
+
+function getMaxLayer(){
+    const temp = sprites;
+    let max = 0;
+    for(i = 0; i < temp.length; i++){
+      if(temp[i].layer > max){
+        max = temp[i].layer;  
+      };
+    };
+};
