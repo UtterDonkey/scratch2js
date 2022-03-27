6fb4b6240c80fe3625932c0c85e52b75d5a55fbe
UtterDonkey <68461677+UtterDonkey@users.noreply.github.com>
2022-03-27 12:20:14 +0100

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,7 +1,5 @@
 
 document.body.insertAdjacentHTML('beforeEnd', `<h1 style="
     position: fixed;
-">Test</h1><img src="https://cdn.assets.scratch.mit.edu/internalapi/asset/7e24c99c1b853e52f8e7f9004416fa34.png/get" id="sprite1" style="position: fixed; left: 693px; top: 367px;"></img>`)
-sprite = document.getElementById('sprite1');
+">Test</h1>`)
 
-initSprite(sprite, spriteData);
