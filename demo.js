05a0989ece53248c8c89084e3b912c9b0cb9bbd7
UtterDonkey <68461677+UtterDonkey@users.noreply.github.com>
2022-04-25 17:37:38 +0100

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,4 +1,4 @@
 document.body.insertAdjacentHTML('beforeEnd', `<h1 style="
     position: fixed;
 ">Test</h1>`);
-demoSprite = createSprite();
+demoSprite = createSprite('https://cdn.assets.scratch.mit.edu/internalapi/asset/7e24c99c1b853e52f8e7f9004416fa34.png/get');
