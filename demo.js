423af2a10b6f6c3dbb0aac066c165678c7afffc3
UtterDonkey <68461677+UtterDonkey@users.noreply.github.com>
2022-07-12 18:25:41 +0100

    Update demo.js

--- demo.js
+++ demo.js
@@ -2,3 +2,8 @@ document.body.insertAdjacentHTML('beforeEnd', `<h1 style="
     position: fixed;
 ">Test</h1>`);
 demoSprite = createSprite('https://cdn.assets.scratch.mit.edu/internalapi/asset/7e24c99c1b853e52f8e7f9004416fa34.png/get');
+function removeDemoSprite(){
+    clearTimeout(getSprite(demoSprite).renderProcess);
+    document.getElementById('sprite#' + demoSprite).remove();
+    demoSprite = undefined;
+};
