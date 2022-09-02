5432c569ccbb17342900bc8290951ca1c2908617
UtterDonkey <68461677+UtterDonkey@users.noreply.github.com>
2022-09-02 17:52:08 +0100

    Update sprites.js

--- sprites.js
+++ sprites.js
@@ -9,7 +9,7 @@ function updateSprite(spriteID, data){
     spriteID.style.left = x - (spriteID.clientWidth/2);
     spriteID.style.top = y - (spriteID.clientHeight/2);
     spriteID.style.display = data.show ? '' : 'none';
-    spriteID.style.transform = `rotate(${data.direction-90}deg) rotateX(${data['3d'].rotateY-90}deg) rotateY(${data['3d'].rotateX-90}deg) scale(${(data.size/100)*((globalValues.getFixedValue('width')+globalValues.getFixedValue('height'))/2)})`;
+    spriteID.style.transform = `rotate(${data.direction-90}deg) rotateX(${data['3d'].rotateY-90}deg) rotateY(${data['3d'].rotateX-90}deg) scale(${(data.size/100)})`;
     spriteID.style.opacity = data.opacity/100;
 };
 
