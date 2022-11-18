ccda17fa7e4bcf851efbdcddfc7822009ce2e7e6
UtterDonkey <68461677+UtterDonkey@users.noreply.github.com>
2022-11-18 10:48:24 +0000

    Update sprites.js

--- sprites.js
+++ sprites.js
@@ -6,6 +6,7 @@ function updateSprite(spriteID, data){
     let x = (data.x*globalFunctions.getFixedValue('width')) + (window.innerWidth/2);
     let y = -(data.y*globalFunctions.getFixedValue('height')) + (window.innerHeight/2);
     spriteID.style.position = 'relative';
+    spriteID.style.pointerEvents = 'none';
     spriteID.style.left = x - (spriteID.clientWidth/2) + 'px';
     spriteID.style.top = y - (spriteID.clientHeight/2) + 'px';
     spriteID.style.display = data.hidden ? 'none' : '';
